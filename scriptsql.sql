3d3dbe343ce15cda7f184c70c19dee534b667b4d
kne2 <fcanedoromanos@gmail.com>
2021-09-29 01:28:21 -0300

    script de sql con grupos/asignaturas

--- scriptsql.sql
+++ scriptsql.sql
@@ -54,7 +54,6 @@ create table asignatura(
 create table grupoTieneAsignatura(
     nombreGrupo varchar(3),
     nombreAsignatura varchar(15),
-    userId varchar(8),
     PRIMARY KEY (nombreGrupo, nombreAsignatura)
 );
 
@@ -69,6 +68,7 @@ create table docenteAnotaAsignatura(
     nombreAsignatura varchar(15),
     PRIMARY KEY (userId, nombreAsignatura)
 );
+<<<<<<< HEAD
 
 create table chat(
     chatId int AUTO_INCREMENT PRIMARY KEY,
@@ -109,6 +109,48 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON test.mensaje TO "alumno"@"localhost";
 GRANT SELECT, INSERT, UPDATE, DELETE ON test.chat TO "alumno"@"localhost";
 GRANT SELECT, INSERT, UPDATE, DELETE ON test.docenteAnotaAsignatura TO "alumno"@"localhost";
 
+=======
+
+create table chat(
+    chatId int AUTO_INCREMENT PRIMARY KEY,
+    userId varchar(8) NOT NULL,
+    nombreAsignatura varchar(15) NOT NULL,
+    fecha DATETIME NOT NULL,
+    resuelto ENUM("false", "true") NOT NULL,
+    FOREIGN KEY (nombreAsignatura) REFERENCES asignatura(nombreAsignatura),
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+create table mensaje(
+    mensajeId int AUTO_INCREMENT PRIMARY KEY,
+    chatId int NOT NULL,
+    userId varchar(8) NOT NULL,
+    fecha DATETIME NOT NULL,
+    contenido varchar(500),
+    FOREIGN KEY (userId) REFERENCES user(id),
+    FOREIGN KEY (chatId) REFERENCES chat(chatId)
+);
+
+create table login(
+    userId varchar(8) PRIMARY KEY,
+    fechaLogIn DATETIME NOT NULL,
+    fechaLogOut DATETIME NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+/* Permisos de  alumno */
+GRANT SELECT, INSERT, UPDATE ON test.user TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE ON test.consulta TO "alumno"@"localhost";
+GRANT SELECT, UPDATE ON test.respuesta TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE ON test.grupo TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE ON test.asignatura TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE ON test.grupoTieneAsignatura TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE ON test.alumnoAnotaGrupo TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE, DELETE ON test.mensaje TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE, DELETE ON test.chat TO "alumno"@"localhost";
+GRANT SELECT, INSERT, UPDATE, DELETE ON test.docenteAnotaAsignatura TO "alumno"@"localhost";
+
+>>>>>>> 0e12289e69310f80e7e7117574e393d6fc05bb34
 /* Permisos de  docente */
 GRANT SELECT, INSERT, UPDATE ON test.user TO "docente"@"localhost";
 GRANT SELECT, UPDATE ON test.consulta TO "docente"@"localhost";
@@ -172,4 +214,59 @@ INSERT INTO asignatura(nombreAsignatura) VALUES ("Geometria2");
 INSERT INTO asignatura(nombreAsignatura) VALUES ("Electronica");
 INSERT INTO asignatura(nombreAsignatura) VALUES ("Taller1");
 INSERT INTO asignatura(nombreAsignatura) VALUES ("Taller2");
+<<<<<<< HEAD
 INSERT INTO asignatura(nombreAsignatura) VALUES ("Logica");
+
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "Programacion1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "Ingles1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "SO1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "Matematica1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "Apt1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "Geometria1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "Taller1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BF", "Logica");
+
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "Programacion1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "Ingles1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "SO1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "Matematica1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "Apt1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "Geometria1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "Taller1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("1BD", "Logica");
+
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Programacion1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Diseñoweb1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "BBDD1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Ingles2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "SO2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Matimatica2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Apt2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Geometria2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Electronica");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BD", "Taller2");
+
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Programacion1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Diseñoweb1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "BBDD1");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Ingles2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "SO2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Matimatica2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Apt2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Geometria2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Electronica");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("2BF", "Taller2");
+
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "Programacion3");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "Diseñoweb2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "BBDD2");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "Ingles3");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "SO3");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "Matematica3");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "ADA");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "Proyecto");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "Sociologia");
+INSERT INTO grupoTieneAsignatura(nombreGrupo, nombreAsignatura) VALUES ("3BB", "Filosofia");
+=======
+INSERT INTO asignatura(nombreAsignatura) VALUES ("Logica");
+>>>>>>> 0e12289e69310f80e7e7117574e393d6fc05bb34
